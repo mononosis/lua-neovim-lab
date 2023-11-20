0f4fd50391ee3a50b6179438b7a186cd210e974b
Your Name <you@example.com>
2023-11-20 05:45:39 +0000

    first config

---
+++ init.lua
@@ -0,0 +1,23 @@
+local lspconfig = require('lspconfig')
+-- Configure lua-lsp
+lspconfig.lua_ls.setup {
+    cmd = { "lua-language-server" },
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT', -- Use 'LuaJIT' or 'Lua5.1'
+                path = vim.split(package.path, ';'),
+            },
+            diagnostics = {
+                enable = true,
+                globals = { 'vim', 'nvim' },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                },
+            },
+        },
+    },
+}
