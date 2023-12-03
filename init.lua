a4aa8a7eaac9ddddddbec36c595c564d8f99e352
Your Name <you@example.com>
2023-12-03 09:39:13 +0000

    test

--- init.lua
+++ init.lua
@@ -1,28 +1,68 @@
+vim.opt.autoindent = false
+vim.o.smartindent = false
+
+
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Configure lua-lsp
 lspconfig.lua_ls.setup {
-    cmd = { "lua-language-server" },
-    settings = {
+  on_attach = function(_, bufnr)
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.expandtab = true
+
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>',
+      { noremap = true, silent = true })
+  end,
+  cmd = { "lua-language-server" },
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
-            runtime = {
-                version = 'LuaJIT', -- Use 'LuaJIT' or 'Lua5.1'
-                path = vim.split(package.path, ';'),
-            },
-            diagnostics = {
-                enable = true,
-                globals = { 'vim', 'nvim' },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                },
-            },
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',         -- Use 'LuaJIT' or 'Lua5.1'
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        enable = true,
+        globals = { 'vim', 'nvim' },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
         },
+      },
     },
-    capabilities = capabilities
+  },
+  capabilities = capabilities
 }
 
-require('change-color')
 
+require('change-color')
