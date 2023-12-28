a26bedf4c37d309eeed2d98346e77e508a64aa76
Your Name <you@example.com>
2023-12-28 15:34:06 +0000

    test

--- lua/nvim-lua-config/init.lua
+++ lua/nvim-lua-config/init.lua
@@ -1,10 +1,6 @@
 local utils = require("nvim-utils-config")
--- Function to get the directory of the current script
-
--- Get the current directory of the script
 local current_dir = utils.this_file_path()
 utils.set_packages_paths(current_dir)
-
 require('lua-theme').setup()
 require('lua-options').setup()
 require('lua-editor').setup()
